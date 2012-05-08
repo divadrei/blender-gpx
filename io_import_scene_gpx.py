f9b338f1eadc0cc57ffd21e96084bcf19574ccb7
David Charbonnier <d.charbonnier@oxys.net>
2012-05-08 20:31:07 +0200

    center

--- io_import_scene_gpx.py
+++ io_import_scene_gpx.py
@@ -26,6 +26,13 @@ class ImportGPX(bpy.types.Operator, ImportHelper):
 	filter_glob = bpy.props.StringProperty(default="*.gpx", options={'HIDDEN'})
 
 	def execute(self, context):
+		min_x = None
+		min_y = None
+		min_z = None
+		max_x = None
+		max_y = None
+		max_z = None
+		
 		dom1 = parse(self.properties.filepath)
 		verts = []
 		for t in dom1.getElementsByTagName('trkpt'):
@@ -36,8 +43,31 @@ class ImportGPX(bpy.types.Operator, ImportHelper):
 			x = math.sin(lat) * math.cos(lon) * alt
 			y = math.sin(lat) * math.sin(lon) * alt
 			z = math.cos(lat) * alt
+
+			if min_x is None or x < min_x:
+				min_x = x
+			if min_y is None or y < min_y:
+				min_y = y
+			if min_z is None or z < min_z:
+				min_z = z
+
+			if max_x is None or x > max_x:
+				max_x = x
+			if max_y is None or y > max_y:
+				max_y = y
+			if max_z is None or z > max_z:
+				max_z = z
+				
 			verts.append((x,y,z))
 		
+		offset_x = max_x - (max_x-min_x) / 2
+		offset_y = max_y - (max_y-min_y) / 2
+		offset_z = max_z - (max_z-min_z) / 2				
+		
+		
+		for i in range(0,len(verts)):
+			verts[i]=(verts[i][0]-offset_x,verts[i][1]-offset_y,verts[i][2]-offset_z)
+			
 		mesh = bpy.data.meshes.new("trace")
 		object = bpy.data.objects.new("trace", mesh)
 		object.location = bpy.context.scene.cursor_location
